1eac07163a6e7c22be24bcca79840e7489b0a259
Briseid Pagador <bpagador@gmail.com>
2020-05-01 14:10:04 -0700

    Updates

---
+++ precache-manifest.cd580c20ffac825878a434e5c681a203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d50b8a36bc95e9c53115397d2910fb1",
+    "url": "/rick-and-morty-1/index.html"
+  },
+  {
+    "revision": "4967870a12e9990ab6fe",
+    "url": "/rick-and-morty-1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eda634d61f5668174187",
+    "url": "/rick-and-morty-1/static/js/2.6ee80080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rick-and-morty-1/static/js/2.6ee80080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4967870a12e9990ab6fe",
+    "url": "/rick-and-morty-1/static/js/main.2d48398b.chunk.js"
+  },
+  {
+    "revision": "a57f1dae2d5247d7ece7",
+    "url": "/rick-and-morty-1/static/js/runtime-main.dd6f33b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rick-and-morty-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
